4fb5e9a3e6a1f7ba11670124dfd3590d6d8b5e42
Takuya Narihira <dvbscb@gmail.com>
2013-03-31 02:00:33 +0900

    add an interface for selecting language code

--- youtube2srt.py
+++ youtube2srt.py
@@ -1,5 +1,9 @@
 #! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import sys, re, urllib2
+from xml.dom import minidom
+
 CAPTION_LANG_LIST_URL = "http://video.google.com/timedtext?type=list&v=%s"
 CAPTION_URL = "http://video.google.com/timedtext?hl=%(lang_code)s&lang=%(lang_code)s&name=%(name)s&v=%(video_id)s"
 
@@ -8,8 +12,33 @@ def main(url):
     # get caption languages list
     langUrl = CAPTION_LANG_LIST_URL%(videoId)
     page = urllib2.urlopen(langUrl)
-    for line in page:
-        print line
+    langDom = minidom.parse(page)
+#    print langDom.toxml()
+    tracks = langDom.getElementsByTagName('track')
+    while True:
+        print 'Here are the available caption langages'
+        for track in tracks:
+            langId =  int(track.getAttribute('id'))
+            langCode = track.getAttribute('lang_code')
+            langDefault = track.getAttribute('lang_default')
+            langOrig = track.getAttribute('lang_original')
+            langTrasl = track.getAttribute('lang_translated')
+            name = track.getAttribute('name')
+            print ' %d) lang_code=%s'%(langId, langCode)
+        try:
+            selectedId = int(raw_input('Select number:'))
+        except ValueError:
+            print >> sys.stderr, 'Input number.'
+            continue
+        if (selectedId > len(tracks)) or (selectedId < 0):
+            print >> sys.stderr, '!Input correct number'
+            continue
+        break
+    track = tracks[selectedId]
+    langCode = track.getAttribute('lang_code')
+    print langCode
+
+
 
 def get_video_id(url):
     """
