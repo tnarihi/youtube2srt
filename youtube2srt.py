81b92929f0fc606e147a309fe7dfaffd19057c45
Takuya Narihira <dvbscb@gmail.com>
2013-03-31 02:19:43 +0900

    refactor

--- youtube2srt.py
+++ youtube2srt.py
@@ -7,39 +7,6 @@ from xml.dom import minidom
 CAPTION_LANG_LIST_URL = "http://video.google.com/timedtext?type=list&v=%s"
 CAPTION_URL = "http://video.google.com/timedtext?hl=%(lang_code)s&lang=%(lang_code)s&name=%(name)s&v=%(video_id)s"
 
-def main(url):
-    videoId = get_video_id(url)
-    # get caption languages list
-    langUrl = CAPTION_LANG_LIST_URL%(videoId)
-    page = urllib2.urlopen(langUrl)
-    langDom = minidom.parse(page)
-#    print langDom.toxml()
-    tracks = langDom.getElementsByTagName('track')
-    while True:
-        print '\nHere are the available caption langages:'
-        for track in tracks:
-            langId =  int(track.getAttribute('id'))
-            langCode = track.getAttribute('lang_code')
-            langDefault = track.getAttribute('lang_default')
-            langOrig = track.getAttribute('lang_original')
-            langTrasl = track.getAttribute('lang_translated')
-            name = track.getAttribute('name')
-            print ' %d) %s'%(langId, langCode)
-        try:
-            selectedId = int(raw_input('Select number:'))
-        except ValueError:
-            print >> sys.stderr, 'Input number'
-            continue
-        if (selectedId > len(tracks)-1) or (selectedId < 0):
-            print >> sys.stderr, 'Input correct number'
-            continue
-        break
-    track = tracks[selectedId]
-    langCode = track.getAttribute('lang_code')
-    print langCode
-
-
-
 def get_video_id(url):
     """
     Get 'v' option value
@@ -51,6 +18,44 @@ def get_video_id(url):
         print >> sys.stderr, 'cannot parse youtube video id.'
         sys.exit(1)
     
+def get_lang_attrs(trackDom):
+    keys = trackDom.attributes.keys()
+    r = {}
+    for key in keys:
+        r[key] = trackDom.getAttribute(key)
+    return r
+
+def get_lang_list(videoId):
+    langUrl = CAPTION_LANG_LIST_URL%(videoId)
+    page = urllib2.urlopen(langUrl)
+    langDom = minidom.parse(page)
+    tracks = langDom.getElementsByTagName('track')
+    langList = [get_lang_attrs(track) for track in tracks]
+    return langList
+
+def select_lang(langList):
+    while True:
+        print '\nHere are the available caption langages:'
+        for lang in langList:
+            print ' %s) %s'%(lang['id'], lang['lang_code'])
+        try:
+            selectedId = int(raw_input('Select number:'))
+        except ValueError:
+            print >> sys.stderr, 'Error: Input number'
+            continue
+        if (selectedId > len(langList)-1) or (selectedId < 0):
+            print >> sys.stderr, 'Error: Input correct number'
+            continue
+        break
+    langCode = langList[selectedId]['lang_code']
+    return langCode
+
+def main(url):
+    videoId = get_video_id(url)
+    langList = get_lang_list(videoId)
+    langCode = select_lang(langList)
+    print langCode
+
 if __name__ == '__main__':
 
     av = sys.argv
