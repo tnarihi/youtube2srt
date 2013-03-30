0622758042717efb1dbbc0f3c80d3a05456b14c2
Takuya Narihira <dvbscb@gmail.com>
2013-03-31 03:36:11 +0900

    modify to print captions with SRT format

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Youtube caption to SRT
 This is a script that helps download YouTube caption files and convert them to SRT format.
 This is just for my practice of parsing XML files using Python. 
+
+##USAGE
+* Go www.youtube.com and watch a movie.
+* Copy the video URL.
+* Run this script like below.
+    ./youtube2srt.py <video url>
+* Then you can see the available caption language list.
+* Run this script again with the index number of caption language you want to dump.
+    ./youtube2srt.py <video url> <lang id> > script.srt
--- youtube2srt.py
+++ youtube2srt.py
@@ -3,6 +3,7 @@
 
 import sys, re, urllib2
 from xml.dom import minidom
+from HTMLParser import HTMLParser
 
 CAPTION_LANG_LIST_URL = "http://video.google.com/timedtext?type=list&v=%s"
 CAPTION_URL = "http://video.google.com/timedtext?hl=%(lang_code)s&lang=%(lang_code)s&name=%(name)s&v=%(video_id)s"
@@ -19,11 +20,7 @@ def get_video_id(url):
         sys.exit(1)
     
 def get_lang_attrs(trackDom):
-    keys = trackDom.attributes.keys()
-    r = {}
-    for key in keys:
-        r[key] = trackDom.getAttribute(key)
-    return r
+    return dict(trackDom.attributes.items())
 
 def get_lang_list(videoId):
     langUrl = CAPTION_LANG_LIST_URL%(videoId)
@@ -47,19 +44,74 @@ def select_lang(langList):
             print >> sys.stderr, 'Error: Input correct number'
             continue
         break
-    langCode = langList[selectedId]['lang_code']
-    return langCode
+    lang = langList[selectedId]
+    return lang
 
 def main(url):
     videoId = get_video_id(url)
     langList = get_lang_list(videoId)
-    langCode = select_lang(langList)
-    print langCode
+
+def print_lang_list(url):
+    videoId = get_video_id(url)
+    langList = get_lang_list(videoId)
+    print '[Language list]'
+    for lang in langList:
+        print '%s) %s'%(lang['id'], lang['lang_code'])
+
+
+def convert_time_format(ftime):
+    return '%02d:%02d:%02d,%03d'%(
+        int(ftime/3600),
+        int(ftime/60)%60,
+        (int(ftime)%3600)%60,
+        int((ftime - int(ftime)) * 1000)
+        )
+
+def print_srt(url, num):
+    videoId = get_video_id(url)
+    langList = get_lang_list(videoId)
+    lang = langList[num]
+    captionUrl = CAPTION_URL%(dict(video_id=videoId, **lang))
+    capDom = minidom.parse(
+        urllib2.urlopen(captionUrl)
+        )
+    texts = capDom.getElementsByTagName('text')
+    hp = HTMLParser()
+    for i, text in enumerate(texts):
+        fstart = float(text.getAttribute('start'))
+        start = convert_time_format(fstart)
+        fdur = float(text.getAttribute('dur'))
+        dur = convert_time_format(fstart+fdur)
+        t = text.childNodes[0].data
+        print '%d'%(i)
+        print '%s --> %s'%(start, dur)
+        print hp.unescape(t)
+
+    
+def print_usage_and_exit():
+    print >> sys.stderr, """#Print YouYube video caption with SRT format
+
+##USAGE
+* Go www.youtube.com and watch a movie.
+* Copy the video URL.
+* Run this script as below.
+    %(prog)s <video url>
+* Then you can see the available caption language list.
+* Run this script again with the index number of caption language you want to dump.
+    %(prog)s <video url> <lang id> > script.srt
+"""%{'prog':av[0]}
+    sys.exit(1)
 
 if __name__ == '__main__':
 
     av = sys.argv
-    if len(av) != 2:
-        print >> sys.stderr, 'usage: %s <Youtube video URL>'%av[0]
-        sys.exit(1)
-    main(av[1])
+    if len(av) == 2:
+        print_lang_list(av[1])
+    elif len(av) == 3:
+        try:
+            langId = int(av[2])
+        except ValueError:
+            print_usage_and_exit()
+        print_srt(av[1], langId)
+    else:
+        print_usage_and_exit()
